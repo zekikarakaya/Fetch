f5288776333492f99437b74ab0579fb440b85480
Zeki Karakaya <zeki1907801@gmail.com>
2024-02-05 09:38:39 +0200

    dosya ekledim

--- 1/src/index.js
+++ 1/src/index.js
@@ -11,11 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    
   </React.StrictMode>
 );
 
